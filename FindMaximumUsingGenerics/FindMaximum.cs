41a020d36b58e0af22635d9e91e4dba315d8fb27
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-29 20:46:31 +0530

    UC1.1 Test Case Performed

---
+++ FindMaximumUsingGenerics/FindMaximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class FindMaximum
+    {
+        public static int FindIntMaximum(int num1,int num2, int num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            else
+            {
+                return num3;
+            }
+        }
+
+    }
+}
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find Maximum Using Generics!");
+            //UC1 - Maximum int Number
+            //Test Case 1 - Max Number at first position and return same. 
+            int maxInt = FindMaximum.FindIntMaximum(32,18,11);
+            Console.WriteLine("Maximum Numbe is : " + maxInt);
         }
     }
 }
