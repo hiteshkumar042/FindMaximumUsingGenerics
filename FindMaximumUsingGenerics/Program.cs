ff545e07402ff7aa2e16db648261643d265b39ea
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-29 20:54:31 +0530

    UC1 Test Case 1.2 performed

--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -7,8 +7,11 @@
             Console.WriteLine("Welcome to Find Maximum Using Generics!");
             //UC1 - Maximum int Number
             //Test Case 1 - Max Number at first position and return same. 
-            int maxInt = FindMaximum.FindIntMaximum(32,18,11);
-            Console.WriteLine("Maximum Numbe is : " + maxInt);
+            int max1stInt = FindMaximum.FindIntMaximum(32,18,11);
+            Console.WriteLine("Maximum Number for 1st test Case is : " + max1stInt);
+            //Test Case 1 - Max Number at Second position and return same. 
+            int max2ndInt = FindMaximum.FindIntMaximum(18, 32, 11);
+            Console.WriteLine("Maximum Number for 2nd test Case is : " + max2ndInt);
         }
     }
 }
