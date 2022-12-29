213e8f948740c93c6f1c8e4dd1e9320b0be42146
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-29 20:27:04 +0530

    Welcome Message

---
+++ FindMaximumUsingGenerics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FindMaximumUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Using Generics!");
+        }
+    }
+}
